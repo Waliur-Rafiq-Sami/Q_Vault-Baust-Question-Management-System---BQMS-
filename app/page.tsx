819cb59d0dff28a82e0c2e67e4fcb1f51b400a8e
SAM's pc <Waliur Rafiq Sami>
2026-04-23 00:48:55 +0600

    check main branch

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const page = () => {
-  return <div className="border border-blue-200">sami</div>;
+  return (
+    <div className="border border-blue-200">
+      <p>sami</p>
+    </div>
+  );
 };
 
 export default page;
