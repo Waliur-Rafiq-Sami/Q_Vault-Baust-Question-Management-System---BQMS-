5fc85e8fd303f7d449bbe8aa5dec1c30e5a8aea9
Md. Tamim Hasan Siam <92130824+tamim-hasan-01@users.noreply.github.com>
2026-05-04 16:34:39 +0600

    3dot menu, bookmark ui update,navbar ui update

--- lib/utils/downloadFile.ts
+++ lib/utils/downloadFile.ts
@@ -1,5 +1,18 @@
 import JSZip from "jszip";
 
+export const downloadFile = async (url: string, filename: string) => {
+  const response = await fetch(url);
+  const blob = await response.blob();
+  const blobUrl = window.URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  link.href = blobUrl;
+  link.download = filename;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  window.URL.revokeObjectURL(blobUrl);
+};
+
 export const downloadAllFiles = async (urls: string[], folderName: string) => {
   const zip = new JSZip();
   const folder = zip.folder(folderName);
